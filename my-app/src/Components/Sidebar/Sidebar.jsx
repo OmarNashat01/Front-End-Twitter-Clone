26c42814e63692c7c5b66ee75da36b7fea00f921
helloworld877 <mmghaly3@gmail.com>
2022-04-02 17:33:51 +0200

    navbar routes
    added routes

--- my-app/src/Components/Sidebar/Sidebar.jsx
+++ my-app/src/Components/Sidebar/Sidebar.jsx
@@ -19,27 +19,30 @@ function Sidebar() {
 
         <li className="nav-item">
           {/* HOME */}
-          <SidebarOption tabName="Home" />
+          <SidebarOption tabName="Home" pageRoute="/" />
         </li>
         <li className="nav-item">
           {/* EXPLORE */}
-          <SidebarOption tabName="Explore" />
+          <SidebarOption tabName="Explore" pageRoute="/explore" />
         </li>
         <li className="nav-item">
           {/* NOTIFICATIONS */}
-          <SidebarOption tabName="Notifications" />
+          <SidebarOption tabName="Notifications" pageRoute="/notifications" />
         </li>
         <li className="nav-item">
           {/* BOOKMARKS */}
-          <SidebarOption tabName="Bookmarks" />
+          <SidebarOption tabName="Bookmarks" pageRoute="/bookmarks" />
         </li>
         <li className="nav-item">
           {/* PROFILE */}
-          <SidebarOption tabName="Profile" />
+          <SidebarOption tabName="Profile" pageRoute="/profile" />
         </li>
         <li className="nav-item">
           {/* TWEET BUTTON*/}
-          <button className="btn btn-primary rounded-pill tweet-btn ">
+          <button className="btn btn-primary  tweet-btn tweet-btn-small  d-md-block d-xl-none d-lg-none rounded-circle">
+            Tweet
+          </button>
+          <button className="btn btn-primary rounded-pill tweet-btn d-none d-lg-block d-xl-block ">
             Tweet
           </button>
         </li>
--- my-app/src/Components/Sidebar/SidebarOption.jsx
+++ my-app/src/Components/Sidebar/SidebarOption.jsx
@@ -8,24 +8,31 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import PersonIcon from "@mui/icons-material/Person";
 
-function test() {
-  console.log("click");
-}
+//NAVIGATION
+import { useNavigate } from "react-router-dom";
 
 function SidebarOption(props) {
+  let navigate = useNavigate();
+
   return (
     // optionIcon->the name of the used icon
     // optionName->the text displayed beside it
+    //pageRoute->route of the targeted page
+
     <button
       type="button"
-      onClick={test}
+      onClick={() => {
+        navigate(props.pageRoute);
+      }}
       className=" btn nav-option rounded-pill float-parent-element "
       data-mdb-ripple-color="light"
     >
-      <div className="  float-child-element left-child">
+      <div className="  float-child-element left-child ">
         <HomeIcon />
       </div>
-      <div className=" tab-text  float-child-element">{props.tabName}</div>
+      <div className=" tab-text  float-child-element d-none d-lg-block d-xl-block">
+        {props.tabName}
+      </div>
     </button>
   );
 }
