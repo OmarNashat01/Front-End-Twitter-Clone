9b323f8439ab6574d14e142571e413d01fb36b08
MinaM99 <82058247+MinaM99@users.noreply.github.com>
2022-04-03 20:47:56 +0200

    First Commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import TweetBox from "./Components/TweetBox/TweetBox";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         <div className="main-screen col col-md-3 col-lg-4 col-sm-3 sticky-top">
           <Sidebar />
         </div>
-        <div className="col col-md-6 col-lg-4 col-sm-6">column</div>
+        <div className="col col-md-6 col-lg-4 col-sm-6">
+          <TweetBox />
+        </div>
         <div className="col col-md-3 col-lg-4 col-sm-3">Column</div>
       </div>
     </div>
