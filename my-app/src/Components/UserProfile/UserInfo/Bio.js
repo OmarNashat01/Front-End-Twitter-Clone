2cd02f4959621f95bf6d1dd462821ac89c60c46b
SaraYahyaA <75972688+SaraYahyaA@users.noreply.github.com>
2022-04-04 23:09:24 +0200

    phase1
    l

--- my-app/src/Components/UserProfile/UserInfo/Bio.js
+++ my-app/src/Components/UserProfile/UserInfo/Bio.js
@@ -1,8 +1,12 @@
 import react from "react";
 import auxelary from "../../../hoc/Auxelary";
 import "./Bio.css"
+import CalendarMonthTwoToneIcon from '@mui/icons-material/CalendarMonthTwoTone';
 const Bio = (props) => {
-    return ( < div className = "bio" > hii im bio { props.children } < /div>
+    return ( < div className = "bio" > hii im bio { props.children } <
+        div > <
+        CalendarMonthTwoToneIcon / > Joined May 2018 < /div>   < /
+        div >
 
     )
 
