8d290f88e77d6c546d92964361d8a992dcc6caa5
helloworld877 <mmghaly3@gmail.com>
2022-04-05 13:44:36 +0200

    added page toggler

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import AdminPage from "./Components/AdminPage/AdminPage";
-// import Login from "./Components/Login/login";
+import AdminPage from "./Components/AdminPage/AdminPage";
+import Login from "./Components/Login/login";
 
 ReactDOM.render(<App />, document.getElementById("root"));
