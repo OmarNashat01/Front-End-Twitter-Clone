797a9363141e942309399d9a92f9678770d6ef32
MinaM99 <82058247+MinaM99@users.noreply.github.com>
2022-04-05 03:26:13 +0200

    modified Submit Testing

--- my-app/src/Components/TweetBox/__tests__/tweetbox.test.js
+++ my-app/src/Components/TweetBox/__tests__/tweetbox.test.js
@@ -6,16 +6,7 @@ import '@testing-library/jest-dom/extend-expect';
 import TweetBox from '../TweetBox';
 
 
-function createPasteEvent(html) {
-    const text = html.replace('<[^>]*>', '');
-    return {
-      clipboardData: {
-        types: ['text/plain', 'text/html'],
-        getData: (type) => (type === 'text/plain' ? text : html),
-      },
-    };
-}
-
+  
 afterEach(()=>{
     cleanup();
 });
@@ -33,21 +24,17 @@ test('should render tweetbox component',() =>{
  })
 
 
- test('If Text is entered the Submit button is enabled',() => {
-    // render(<TweetBox/>);
-    // const tweetinputElement = screen.getByTestId('tweetinput-1');
-    // userEvent.type(tweetinputElement, "test@mail.com");
-    // const submitElement = screen.getByRole('button',{name: /Tweet/i});
-    // expect(tweetinputElement).toBeInTheDocument;
-    // expect(tweetinputElement).toHaveAttribute("type", "hidden");
-    // expect(submitElement).toBeDisabled();
-    // screen.getByRole('');
-    
-    // const editorNode = render(<TweetBox/>).querySelector(".public-DraftEditor-content");
-    // const eventProperties = createPasteEvent(textToPaste);
-    // const pasteEvent = createEvent.paste(editorNode, eventProperties);
-    // pasteEvent.clipboardData = eventProperties.clipboardData;
-    // fireEvent(editorNode, pasteEvent);
-    // screen.getByRole('');
+ test('If Text is entered the Submit button is enabled',async () => {
+    render(<TweetBox />);
+    const textarea = screen.getByRole('textbox');
+    const event = createEvent.paste(textarea, {
+    clipboardData: {
+        types: ['text/plain'],
+        getData: () => 'MyText',
+    },
+    });
+    fireEvent(textarea, event);
+    const submitElement = screen.getByRole('button',{name: /Tweet/i});
+    expect(submitElement).toBeEnabled();
 
  });
