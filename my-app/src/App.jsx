39ade7db5216c815ce90cd70bad9e647af9a59a6
MinaM99 <82058247+MinaM99@users.noreply.github.com>
2022-04-18 22:28:38 +0200

    Searchbar CSS module addition

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import TweetBox from "./Components/TweetBox/TweetBox";
+import SearchBar from "./Components/SearchBar/SearchBar";
 
 function App() {
   return (
@@ -13,7 +14,9 @@ function App() {
         <div className="col col-md-6 col-lg-4 col-sm-6">
           <TweetBox />
         </div>
-        <div className="col col-md-3 col-lg-4 col-sm-3">Column</div>
+        <div className="col col-md-3 col-lg-4 col-sm-3">
+          <SearchBar />
+        </div>
       </div>
     </div>
   );
