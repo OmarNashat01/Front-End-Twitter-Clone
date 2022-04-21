8ceb6985140a02ec6e3263c6821992e814a02a24
yousef-alaa-mostafa <74533934+yousef-alaa-mostafa@users.noreply.github.com>
2022-04-21 18:05:49 +0200

    updated signup and login
    updated signup and login

--- my-app/src/Components/Login/login.jsx
+++ my-app/src/Components/Login/login.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from "react";
-import "./Login.css"
+import LoginCss from "./Login.module.css"
 import Footer from "./Footer";
 import RightHalf from "./rightHalf";
 import LeftHalf from "./LeftHalf";
 
 function Login() {
   return (
-      <div className="HomePage row">  
-        <div className="col-lg-6 col-md-12 col-sm-12 order-lg-1 order-md-2 order-sm-2 order-2  LeftHalf-div">
+      <div className="row" id={LoginCss.HomePage}>  
+        <div className="col-lg-7 col-md-12 col-sm-12 order-lg-1 order-md-2 order-sm-2 order-2" id={LoginCss.LeftHalfDiv}>
           <LeftHalf  />
         </div>
-        <div className="col-lg-6 col-md-12 col-sm-12 order-lg-2 order-md-1 order-sm-1 order-1  RightHalf-div">
+        <div className="col-lg-5 col-md-12 col-sm-12 order-lg-2 order-md-1 order-sm-1 order-1" id={LoginCss.RightHalfDiv}>
           <RightHalf />
         </div>
         <div className="col-12 order-lg-3 order-md-3 order-sm-3 order-3">
