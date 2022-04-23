d3fc517eb6d2fdf26bb9b043144ec3b85dc2b632
helloworld877 <mmghaly3@gmail.com>
2022-04-23 10:58:21 +0200

    added mocks for signup
    added mocks for the signup flow
    and configured the requests to handle error correctly

--- my-app/src/Api/index.js
+++ my-app/src/Api/index.js
@@ -7,19 +7,32 @@ api.defaults.baseURL = "http://localhost:3030";
 //All request will wait 10 seconds before timeout
 api.defaults.timeout = 10000;
 
+api.defaults.headers.common["x-access-token"] = "AUTH_TOKEN";
+
 export async function getRequest(URL) {
-  const response = await api.get(`/${URL}`);
-  return response;
+  return api
+    .get(`/${URL}`)
+    .then((response) => response)
+    .catch((error) => error.response);
 }
 
 export function postRequest(URL, payload) {
-  return api.post(`/${URL}`, payload).then((response) => response);
+  return api
+    .post(`/${URL}`, payload)
+    .then((response) => response)
+    .catch((error) => error.response);
 }
 
 export function patchRequest(URL, payload) {
-  return api.patch(`/${URL}`, payload).then((response) => response);
+  return api
+    .patch(`/${URL}`, payload)
+    .then((response) => response)
+    .catch((error) => error.response);
 }
 
 export function deleteRequest(URL) {
-  return api.delete(`/${URL}`).then((response) => response);
+  return api
+    .delete(`/${URL}`)
+    .then((response) => response)
+    .catch((error) => error.response);
 }
