2ce146149a1d7ec5ec48b6b06ef662791c5916f6
Arwa-ehab <aroro8900@gmail.com>
2022-05-17 23:13:20 +0200

    added search user functionality

--- my-app/src/Components/AdminPage/SearchUsers/UsersTable/UsersTable.jsx
+++ my-app/src/Components/AdminPage/SearchUsers/UsersTable/UsersTable.jsx
@@ -1,115 +1,133 @@
-import UsersTableStyle from"./UsersTable.module.css"
+import UsersTableStyle from "./UsersTable.module.css"
 import TablePagination from '@mui/material/TablePagination';
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
-import { userColumns,userRows } from "./UsersDate";
+import { userColumns, userRows } from "./UsersDate";
 import { margin } from "@mui/system";
 import { getAllUsers } from "../../../../Api/admin";
-const UsersTable=()=>{
-    const [data, setData] = useState(userRows);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    const [page, setPage] = React.useState(0);
+import{getSearchUsersAdmin}from "../../../../Api/admin";
+const UsersTable = (props) => {
+  const { userName } = props;
+  const [data, setData] = useState(userRows);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
 
-    const [usersLoading,setUsersLoading]=useState(true);
-    const [allUsers,setAllUsers]= useState();
+  const [usersLoading, setUsersLoading] = useState(true);
+  const [allUsers, setAllUsers] = useState();
 
 
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-      };
-      const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-      };
-    
-    const handleDelete = (id) => {
-      setData(data.filter((item) => item.id !== id));
-    };
-  
-    const actionColumn = [
-      {
-        field: "action",
-        headerName: "Action",
-        width: 200,
-        sortable: false,
-        renderCell: (params) => {
-          return (
-            <div className={UsersTableStyle.cellAction}>
-<button   className={UsersTableStyle.banButton} >Ban</button>
-              {/* <div
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      sortable: false,
+      renderCell: (params) => {
+        return (
+          <div className={UsersTableStyle.cellAction}>
+            <button className={UsersTableStyle.banButton} >Ban</button>
+            {/* <div
                 className={UsersTableStyle.banButton}
                 onClick={() => handleDelete(params.row.id)}
               >
                 Ban
               </div> */}
-            </div>
-          );
-        },
+          </div>
+        );
       },
-    ];
+    },
+  ];
 
-    useEffect(() => {
-     
-      const fetchData = async () => {
-        const data = await getAllUsers(setUsersLoading,setAllUsers,`?offset=0&limit=10000`);
+  useEffect(() => {
+
+    const fetchData = async () => {
+      let queryString;
+      if (userName==="")
+      {
+        queryString= `?offset=0&limit=10000`;
+        const data = await getAllUsers(setUsersLoading, setAllUsers, queryString);
+      }
+      else{
+        queryString=`?keyword=${userName}&offset=0&limit=10000`;
+        const data = await getSearchUsersAdmin(setUsersLoading, setAllUsers, queryString);
       }
     
-      // call the function
-      fetchData()
-        // make sure to catch any error
-        .catch(console.error);
-    }, [])
-
-    var allUsersTable;
-    if(!usersLoading)
-    {
-      console.log(allUsers);
-      var id_increment=1;
-       allUsersTable = allUsers.data.users.map(user => ({ id:id_increment++,username: user.username,img: user.prof_pic_url,email:user.email,dateofbirth:user.date_of_birth,followerscount:user.followers_count,followingcount:user.following_count,creationdate:user.creation_date }));
-      console.log(allUsersTable);
-
-
-
     }
 
+    // call the function
+    fetchData()
 
-return (!usersLoading && <div className={UsersTableStyle.dataTable}>
-<DataGrid
-  rows={allUsersTable }
-  columns={userColumns}
-  autoPageSize
-rowsPerPageOptions={[4,5,7]}
-//onPageChange={()=>{userRows.append}}
- onRowsPerPageChange={handleChangeRowsPerPage}
-//  checkboxSelection
-  sx={{
-    boxShadow: '5px 5px 5px grey',
-    border: 2,
-    padding:0.5,
-    backgroundColor:'white',
-    borderColor: 'primary.light',
-    '& .MuiDataGrid-cell:hover': {
-      color: 'primary.main',
-    },
-    '& .columnHeader': {
-      backgroundColor: 'rgba(10, 161, 221,0.8)',
-      
-      fontFamily: 'Arial Black,sans-serif',
-      fontSize:'14px',
-      fontWeight:'700px',
-      letterSpacing:'1.5px',
-     padding:'10px',
-color:'rgba(23, 40, 69)',
-    },
-  }}
- //count={userRows.length}
-//rowsPerPage={rowsPerPage}
- //page={page}
-//onPageChange={handleChangePage}
+      // make sure to catch any error
+      .catch(console.error);
+  }, [userName])
 
-/>
+  var allUsersTable;
 
-</div>);
+  if (!usersLoading) {
+    console.log(allUsers);
+
+    var id_increment = 1;
+    if(allUsers.status===204)
+    {
+      allUsersTable=[];
+    }
+else  {  allUsersTable = allUsers.data.users.map(user => ({ id: id_increment++, username: user.username, img: user.prof_pic_url, email: user.email, dateofbirth: user.date_of_birth, followerscount: user.followers_count, followingcount: user.following_count, creationdate: user.creation_date }));
+    console.log(allUsersTable);}
+
+
+
+  }
+
+
+  return (!usersLoading && <div className={UsersTableStyle.dataTable}>
+    <DataGrid
+      rows={allUsersTable}
+      columns={userColumns}
+      autoPageSize
+      rowsPerPageOptions={[4, 5, 7]}
+      //onPageChange={()=>{userRows.append}}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+      //  checkboxSelection
+      sx={{
+        boxShadow: '5px 5px 5px grey',
+        border: 2,
+        padding: 0.5,
+        backgroundColor: 'white',
+        borderColor: 'primary.light',
+        '& .MuiDataGrid-cell:hover': {
+          color: 'primary.main',
+        },
+        '& .columnHeader': {
+          backgroundColor: 'rgba(10, 161, 221,1)',
+
+          fontFamily: 'Arial Black,sans-serif',
+          fontSize: '14px',
+          fontWeight: '700px',
+          letterSpacing: '0.5px',
+          padding: '10px',
+          color: 'rgba(23, 40, 69)',
+        },
+      }}
+    //count={userRows.length}
+    //rowsPerPage={rowsPerPage}
+    //page={page}
+    //onPageChange={handleChangePage}
+
+    />
+
+  </div>);
 
 }
 export default UsersTable
