cacb7389525bc35d3b20147a59a64c4e2b032c70
MinaM99 <82058247+MinaM99@users.noreply.github.com>
2022-05-19 03:17:09 +0200

    SearchBar Modified

--- my-app/src/Components/Widgetbar/SearchBar/SearchBar.jsx
+++ my-app/src/Components/Widgetbar/SearchBar/SearchBar.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import SearchBarCSS from './SearchBar.module.css';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+import { useEffect, useState } from "react";
 
 
 
 function SearchBar() { 
-
+  const [text,setText]= useState('');
   let textInput = React.createRef();
 
+  const handleText = (event) => {
+    setText(event.target.value);
+};
   function goToSearchRoute() {
     window.open(`/search/${textInput.current.value}`, "_self");
+  }
+  function ClearInput(){
+    setText('')
   }
   
   return (
       <div className={SearchBarCSS.search__input}>
-        <input ref={textInput} className={SearchBarCSS.search__inputtext} placeholder="Search Twitter" type="text"  onKeyPress={(e) => e.key === 'Enter' && goToSearchRoute()}/>
+        {text != '' &&
+        <button className={SearchBarCSS.close__btn} onClick={ClearInput}>
+          <CloseIcon className={SearchBarCSS.close__icon} fontSize="medium"/>
+        </button>
+        }
+        <input ref={textInput} value={text} className={SearchBarCSS.search__inputtext} placeholder="Search Twitter" type="text"  onKeyPress={(e) => e.key === 'Enter' && goToSearchRoute()} onChange={handleText}/>
         <SearchIcon className={SearchBarCSS.search__searchIcon} />
       </div>
   )
