440a06c25400623a796d572ffbf9f44d955fe9e7
SaraYahyaA <75972688+SaraYahyaA@users.noreply.github.com>
2022-05-20 20:51:25 +0200

    pic popup

--- my-app/src/Components/UserProfile/UserProfile.js
+++ my-app/src/Components/UserProfile/UserProfile.js
@@ -16,86 +16,134 @@ import { getMe } from "../../Api/UserProfile";
 import ProfileFeed from "../HomeFeed/ProfileFeed";
 import FollowingPage from "./UserInfo/FollowigPage";
 import bell from "../../assets/bell.png";
+import CloseButton from "react-bootstrap/CloseButton";
 import envelope from "../../assets/envelope-icon-14.png";
 import dots from "../../assets/3dots.jpg";
 
 // console.log("classes");
 const UserProfile = () => {
-  const [buttonPopup, setButtonPopup] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [userData, setUserData] = useState();
-  const imageClick = () => {};
+    const [buttonPopup, setButtonPopup] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [userData, setUserData] = useState();
+    const imageClick = () => {};
+    const [fullScreen, setfullScreen] = useState(false);
+    const [fullScreenImg, setfullScreenImg] = useState("");
 
-  useEffect(() => {
-    const getCurrentUser = async () => {
-      const data = await getMe(setLoading, setUserData);
-    };
-    getCurrentUser();
-  }, []);
+    function setImg(url) {
+        setfullScreenImg(url);
+    }
 
-  if (!loading) {
-    console.log(userData);
-    console.log("loading ended");
-    console.log(userData.data.user.cover_pic_url);
-    console.log(userData.data.user.prof_pic_url);
-  }
+    function fullScreenTog() {
+        setfullScreen(!fullScreen);
+    }
 
-  return (
-    !loading && (
-      <Auxelary>
-        <Container>
-          <Header
-            user={userData.data.user.name}
-            tweetnum={userData.data.user.tweet_count}
-          />{" "}
-          <div>
-            <img
-              src={userData.data.user.cover_pic_url}
-              alt="pic"
-              className="img-fluid "
-            />
-          </div>{" "}
-          <div className={UserCSS.ProfilePic} onClick={() => imageClick()}>
-            <img
-              src={userData.data.user.prof_pic_url}
-              alt="pic"
-              className="img-fluid rounded-circle"
-              height={140}
-              width={140}
-            />{" "}
-          </div>{" "}
-          <br />
-          <div className={UserCSS.UserName}>
-            {" "}
-            {userData.data.user.name}{" "}
-          </div>{" "}
-          <div className={UserCSS.UserNamePro}>
-            {" "}
-            {userData.data.user.username}{" "}
-          </div>{" "}
-          <Bio
-            bio={userData.data.user.bio}
-            joindate={userData.data.user.creation_date}
-            fingcount={userData.data.user.following_count}
-            fercount={userData.data.user.followers_count}
-            bdate={userData.data.user.date_of_birth}
-          />{" "}
-          <Button
-            variant="outline-dark"
-            size="sm"
-            className={UserCSS.editbutton}
-            onClick={() => setButtonPopup(true)}
-          >
-            Edit Profile{" "}
-          </Button>
-          <Tabss> </Tabss>{" "}
-          <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-            {" "}
-          </Popup>
-          <ProfileFeed />{" "}
-        </Container>{" "}
-      </Auxelary>
-    )
-  );
+    function closeImg() {
+        // console.log("closseeeeeeeeeeeee");
+        fullScreenTog();
+    }
+
+    useEffect(() => {
+        const getCurrentUser = async() => {
+            const data = await getMe(setLoading, setUserData);
+        };
+        getCurrentUser();
+    }, []);
+
+    if (!loading) {
+        console.log(userData);
+        console.log("loading ended");
+        console.log(userData.data.user.cover_pic_url);
+        console.log(userData.data.user.prof_pic_url);
+    }
+
+    return (!loading && ( <
+            Auxelary >
+            <
+            Container >
+            <
+            Header user = { userData.data.user.name }
+            tweetnum = { userData.data.user.tweet_count }
+            />{" "} <
+            div onClick = {
+                () => {
+                    setImg(userData.data.user.cover_pic_url);
+                    fullScreenTog();
+                }
+            } >
+            <
+            img src = { userData.data.user.cover_pic_url }
+            alt = "pic"
+            className = "img-fluid " /
+            >
+            <
+            /div>{" "} <
+            div className = { UserCSS.ProfilePic }
+            onClick = {
+                () => {
+                    setImg(userData.data.user.prof_pic_url);
+                    fullScreenTog();
+                }
+            } >
+            <
+            img src = { userData.data.user.prof_pic_url }
+            alt = "pic"
+            className = "img-fluid rounded-circle"
+            height = { 140 }
+            width = { 140 }
+            />{" "} < /
+            div > { " " } <
+            br / >
+            <
+            div className = { UserCSS.UserName } > { " " } { userData.data.user.name } { " " } <
+            /div>{" "} <
+            div className = { UserCSS.UserNamePro } > { " " } { userData.data.user.username } { " " } <
+            /div>{" "} <
+            Bio bio = { userData.data.user.bio }
+            joindate = { userData.data.user.creation_date }
+            fingcount = { userData.data.user.following_count }
+            fercount = { userData.data.user.followers_count }
+            bdate = { userData.data.user.date_of_birth }
+            />{" "} <
+            Button variant = "outline-dark"
+            size = "sm"
+            className = { UserCSS.editbutton }
+            onClick = {
+                () => setButtonPopup(true)
+            } >
+            Edit Profile { " " } <
+            /Button> <
+            Tabss > < /Tabss>{" "} <
+            Popup trigger = { buttonPopup }
+            setTrigger = { setButtonPopup } > { " " } <
+            /Popup> <
+            ProfileFeed / > { " " } <
+            div id = "test" > {
+                fullScreen ? ( <
+                    div className = { UserCSS.fullscreen }
+                    id = "full-div" >
+                    <
+                    CloseButton variant = "dark"
+                    // className={PopCSS.innerPro1}
+                    onClick = { closeImg }
+                    />{" "} {
+                    /* <CloseIcon
+                                      className="close-icon"
+                                      onClick={closeImg}
+                                    ></CloseIcon> */
+                } <
+                img className = { UserCSS.imgfull }
+                src = { fullScreenImg }
+                alt = "" / >
+
+
+
+                <
+                /div>  
+            ): null
+        } <
+        /div> < /
+        Container > { " " } <
+        /Auxelary>
+    ));
 };
 export default UserProfile;
