177822fdc96fa0f65baa58002273d43913d830a5
helloworld877 <mmghaly3@gmail.com>
2022-05-22 11:30:50 +0200

    added post and delete for likes and retweets

--- my-app/src/Api/tweetFull.js
+++ my-app/src/Api/tweetFull.js
@@ -1,15 +1,70 @@
-import { postRequest, getRequest } from "./index";
+import { postRequest, getRequest, deleteRequest } from "./index";
 
 export async function getTweet(setLoading, setFulltweet, params) {
-    //params is a string like => ?id=20
-    // console.log(params);
-    setLoading(true);
+  //params is a string like => ?id=20
+  // console.log(params);
+  setLoading(true);
 
-    try {
-        const response = await getRequest(`tweets/tweet_id${params}`);
-        setFulltweet(response);
-    } catch (error) {
-        console.log(error.message);
-    }
-    setLoading(false);
+  try {
+    const response = await getRequest(`tweets/tweet_id${params}`);
+    setFulltweet(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+  setLoading(false);
+}
+
+//TWEET REQUESTS
+export async function postLike(setLoading, setLiked, requestBody) {
+  setLoading(true);
+
+  try {
+    const response = await postRequest("users/likes", requestBody);
+
+    setLiked(response);
+  } catch (error) {
+    console.log(error);
+    setLiked(error);
+  }
+  setLoading(false);
+}
+
+export async function deleteLike(setLoading, setResponse, params) {
+  setLoading(true);
+
+  try {
+    const response = await deleteRequest(`users/likes${params}`);
+    setResponse(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+  setLoading(false);
+}
+
+//RETWEET REQUESTS
+
+export async function postRetweet(setLoading, setRetweet, requestBody) {
+  setLoading(true);
+
+  try {
+    const response = await postRequest("users/retweets", requestBody);
+
+    setRetweet(response);
+  } catch (error) {
+    console.log(error);
+    setRetweet(error);
+  }
+  setLoading(false);
+}
+
+export async function deleteRetweet(setLoading, setResponse, params) {
+  setLoading(true);
+
+  try {
+    const response = await deleteRequest(`users/retweets${params}`);
+    setResponse(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+  setLoading(false);
 }
