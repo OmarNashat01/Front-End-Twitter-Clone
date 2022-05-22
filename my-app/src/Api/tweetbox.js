e4da4e183f768b115318e0ec0e4a2a8af81e2ddf
helloworld877 <mmghaly3@gmail.com>
2022-05-23 00:52:07 +0200

    added the form data requests

--- my-app/src/Api/tweetbox.js
+++ my-app/src/Api/tweetbox.js
@@ -1,11 +1,43 @@
 import { postRequest, getRequest } from "./index";
 
-export async function postUserTweet(setLoading, setTweet, requestBody) {
+// export async function postUserTweet(setLoading, setTweet, requestBody) {
+//   setLoading(true);
+//   let response;
+
+//   try {
+//     response = await postRequest("tweets", requestBody);
+
+//     setTweet(response);
+//   } catch (error) {
+//     console.log(error);
+//     setTweet(error);
+//   }
+//   setLoading(false);
+// }
+
+//POST TWEET
+export async function postTweet(setLoading, setTweet, formdata) {
   setLoading(true);
   let response;
 
   try {
-    response = await postRequest("tweets", requestBody);
+    response = await postRequest("tweets", formdata);
+
+    setTweet(response);
+  } catch (error) {
+    console.log(error);
+    setTweet(error);
+  }
+  setLoading(false);
+}
+
+//POST REPLY
+export async function postReply(setLoading, setTweet, formdata) {
+  setLoading(true);
+  let response;
+
+  try {
+    response = await postRequest("users/comments", formdata);
 
     setTweet(response);
   } catch (error) {
