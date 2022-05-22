5f3d63682f784f2be4783f1354d5bd96324bd96a
helloworld877 <mmghaly3@gmail.com>
2022-05-23 01:00:34 +0200

    Update tweetbox.js

--- my-app/src/Api/tweetbox.js
+++ my-app/src/Api/tweetbox.js
@@ -1,19 +1,19 @@
 import { postRequest, getRequest } from "./index";
 
-// export async function postUserTweet(setLoading, setTweet, requestBody) {
-//   setLoading(true);
-//   let response;
+export async function postUserTweet(setLoading, setTweet, requestBody) {
+  setLoading(true);
+  let response;
 
-//   try {
-//     response = await postRequest("tweets", requestBody);
+  try {
+    response = await postRequest("tweets", requestBody);
 
-//     setTweet(response);
-//   } catch (error) {
-//     console.log(error);
-//     setTweet(error);
-//   }
-//   setLoading(false);
-// }
+    setTweet(response);
+  } catch (error) {
+    console.log(error);
+    setTweet(error);
+  }
+  setLoading(false);
+}
 
 //POST TWEET
 export async function postTweet(setLoading, setTweet, formdata) {
