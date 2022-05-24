6c23aab9fdb71e1f8ce4fa9b3493dd7f4d83589b
helloworld877 <mmghaly3@gmail.com>
2022-05-24 22:16:53 +0200

    Update HomeFeed.jsx

--- my-app/src/Components/HomeFeed/HomeFeed.jsx
+++ my-app/src/Components/HomeFeed/HomeFeed.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from "./Loader";
 import Post from "../Post/Post";
-import { getHomeTweets } from "../../Api/homeFeed";
-
+import { getHomeTweetsAndRetweets } from "../../Api/homeFeed";
 
 function HomeFeed() {
   const [posts, setPosts] = useState([]);
@@ -11,26 +10,21 @@ function HomeFeed() {
   const [hasMore, setHasMore] = useState(false);
   const [page, setPage] = useState(1);
 
-
   useEffect(() => {
-    getHomeTweets(setLoading, setPosts, setHasMore, `?page=1`);
-
-  }, [])
+    getHomeTweetsAndRetweets(setLoading, setPosts, setHasMore, `?page=1`);
+  }, []);
 
   const fetchData = () => {
     setPage(page + 1);
-    getHomeTweets(setLoading, setPosts, setHasMore, `?page=${page}`);
-
+    getHomeTweetsAndRetweets(setLoading, setPosts, setHasMore, `?page=${page}`);
   };
 
   if (!loading) {
     console.log("homeee");
     console.log(posts[0].tweet_id);
-
   }
 
   return (
-
     <div>
       <InfiniteScroll
         className="overflow-hidden"
@@ -41,29 +35,29 @@ function HomeFeed() {
       >
         {posts &&
           posts.map((post, index) => {
-            return <div key={index}>
-              <Post
-                user_id={post.user_id}
-                tweet_id={post.tweet_id}
-                displayName={post.name}
-                username={post.username}
-                verified="1"
-                text={post.text}
-                avatar={post.prof_pic_url}
-                image={post.images}
-                isLiked="false"
-                isRetweet="false"
-                likes={post.like_count}
-                retweets={post.retweet_count}
-                followers={post.followers_count}
-                following={post.following_count}
-                about={post.bio}
-                isReplying="true"
-              ></Post>
-            </div>
-          })
-        }
-
+            return (
+              <div key={index}>
+                <Post
+                  user_id={post.user_id}
+                  tweet_id={post.tweet_id}
+                  displayName={post.name}
+                  username={post.username}
+                  verified="1"
+                  text={post.text}
+                  avatar={post.prof_pic_url}
+                  image={post.images}
+                  isLiked="false"
+                  isRetweet="false"
+                  likes={post.like_count}
+                  retweets={post.retweet_count}
+                  followers={post.followers_count}
+                  following={post.following_count}
+                  about={post.bio}
+                  isReplying="true"
+                ></Post>
+              </div>
+            );
+          })}
       </InfiniteScroll>
     </div>
   );
