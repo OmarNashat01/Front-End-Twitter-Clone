dccc96ffb91b7104e0163e8a4c1400bb4ca09079
helloworld877 <mmghaly3@gmail.com>
2022-05-25 05:54:51 +0200

    fixed user profile

--- my-app/src/Components/HomeFeed/UserFeed.jsx
+++ my-app/src/Components/HomeFeed/UserFeed.jsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from "react";
-import InfiniteScroll from 'react-infinite-scroll-component';
+import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from "./Loader";
 import Post from "../Post/Post";
 import { getMyTweets, getUserTweets } from "../../Api/homeFeed";
 import axios from "axios";
 
 function UserFeed() {
-  const count = 3
+  const count = 3;
   const [posts, setPosts] = useState([]);
   const [response, setResponse] = useState();
   const [loading, setLoading] = useState(true);
   const [hasMore, setHasMore] = useState(false);
-  const [error, setError] = useState(false)
- 
+  const [error, setError] = useState(false);
 
-  
-//   useEffect(()=>{
-//     getMyTweets(setLoading,setPosts);
-//   },[])
+  //   useEffect(()=>{
+  //     getMyTweets(setLoading,setPosts);
+  //   },[])
 
   useEffect(() => {
     const getCurrentUserTweets = async () => {
@@ -33,44 +31,37 @@ function UserFeed() {
 
     getCurrentUserTweets();
   }, []);
-  
- 
 
-
-
- if (!loading) {
-//     console.log(posts[0].images);
-//     console.log(posts[0].images.length);
+  if (!loading) {
+    //     console.log(posts[0].images);
+    //     console.log(posts[0].images.length);
     console.log(posts);
-   }
-
-
-
- 
+  }
 
   return (
     <div>
       {!loading &&
         posts.data.tweets.map((post, index) => {
-          return <div key={index}>
-            <Post
-              displayName={post.name}
-              username={post.username}
-              verified="0"
-              text={post.text}
-              avatar={post.prof_pic_url}
-
-              image={post.images===null? []:post.images}
-              isLiked="false"
-              isRetweet="false"
-              likes={post.like_count}
-              retweets={post.retweet_count}
-            ></Post>
-          </div>
+          return (
+            <div key={index}>
+              <Post
+                displayName={post.name}
+                username={post.username}
+                verified="0"
+                text={post.text}
+                avatar={post.prof_pic_url}
+                image={post.images === null ? [] : post.images}
+                isLiked="false"
+                isRetweet="false"
+                likes={post.like_count}
+                retweets={post.retweet_count}
+                likers={post.liked_by_ids}
+              ></Post>
+            </div>
+          );
         })}
     </div>
   );
 }
 
 export default UserFeed;
-
